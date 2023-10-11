04b095fdec727c926ab737bd037eda95decb3c89
efratdavid <efratdavid8@gmail.com>
2023-10-11 15:47:41 +0300

    modified:   package-lock.json modified:   package.json modified:   src/app/app.module.ts new file:   src/app/code-block-page/code-block-page.component.css new file:   src/app/code-block-page/code-block-page.component.html new file:   src/app/code-block-page/code-block-page.component.ts new file:   src/app/code-block.service.spec.ts new file:   src/app/code-block.service.ts new file:   src/app/page-not-found/page-not-found.component.css new file:   src/app/page-not-found/page-not-found.component.html new file:   src/app/page-not-found/page-not-found.component.ts new file:   src/app/socket.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LobbyPageComponent } from './lobby-page/lobby-page.component';
+import { CodeBlockPageComponent } from './code-block-page/code-block-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LobbyPageComponent
+    LobbyPageComponent,
+    CodeBlockPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/code-block-page/code-block-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-block-page',
+  templateUrl: './code-block-page.component.html',
+  styleUrls: ['./code-block-page.component.css']
+})
+export class CodeBlockPageComponent {
+
+}
